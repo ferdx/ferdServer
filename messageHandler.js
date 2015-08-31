dd7befc6d5a99e9adfaf69bc6259d63929d4e8ad
Timothy Quach <timothyquachbot@gmail.com>
2015-08-31 10:34:59 -0700

    (feat) giffy module

--- messageHandler.js
+++ messageHandler.js
@@ -1,9 +1,11 @@
 var yo = require('./ferd_modules/yo');
 var hi = require('./ferd_modules/hi');
 var bacon = require('./ferd_modules/bacon')
+var giffy = require('./ferd_modules/giffy')
 
 module.exports = {
   yo: yo,
   hi: hi,
-  bacon: bacon
+  bacon: bacon,
+  giffy: giffy
 };
