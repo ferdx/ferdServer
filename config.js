6c316132af636378f850cb1ca90194c3dea9be51
Timothy Quach <timothyquachbot@gmail.com>
2015-09-01 15:49:35 -0700

    (feat) major refactor of megaFerd and added config wrapper class

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * Thin wrapper aroung configuration file. Used in the case config schema changes.
+ * @param {JSON} config [description]
+ */
+var Config = function(config) {
+  this.config = config;
+};
+
+Config.prototype.botKey = function() {
+  return this.config.conf.botKey;
+};
+
+Config.prototype.botModules = function() {
+  return this.config.conf.botModule;
+};
+
+/**
+ * Get a ferd configuration option used to instatiate a new Ferd object
+ * @return {Object} {ferd_modules: '', apiKey: ''}
+ */
+Config.prototype.ferdConfig = function() {
+  var config = {};
+  config['ferd_modules'] = this.config.conf.botModules;
+  config['apiKey'] = this.config.conf.botKey;
+  return config;
+};
+
+module.exports = Config;
--- megaFerd.js
+++ megaFerd.js
@@ -1,13 +1,59 @@
-var Ferd = require('./Ferd');
-var secrets = require('./secrets.js');
+var Ferd = require('./ferd');
+var Config = require('./config');
 
+/**
+ * MegaFerd. Controller of ferds/
+ */
 var MegaFerd = function() {
-  this.ferds = [];
-  for(var i = 0; i < secrets.ferds.length; i++) {
-    console.log(secrets.ferds[i]);
-
-    this.ferds.push(new Ferd(secrets.ferds[i]));
-  }
+  //
+  this.ferds = {};
 };
+
+/**
+ * Takes in a configuration json and decides what to do with it.
+ * If new BotKey, creates new Ferd object
+ * Else, nothing.
+ * @param  {JSON} json See secrets.js
+ * @param {Function} callback
+ */
+MegaFerd.prototype.process = function(json, callback) {
+  callback = callback || function(){};
+  var config = new Config(json);
+  if(this.hasFerd(config)) {
+    console.log('Ferd for this key exists');
+  } else {
+    this.createFerd(config);
+  }
+  callback();
+};
+
+/**
+ * Creates Ferd with config
+ * @param  {Config} config [description]
+ * @return {[type]}        [description]
+ */
+MegaFerd.prototype.createFerd = function(config) {
+  var ferdConfig = config.ferdConfig();
+  var botKey = config.botKey();
+  var ferd = new Ferd(ferdConfig);
+  this.ferds[botKey] = ferd;
+};
+
+/**
+ * Checks if a ferd object with apiKey exists
+ * @param  {Config}  config [description]
+ * @return {Boolean}
+ */
+MegaFerd.prototype.hasFerd = function(config) {
+  var botKey = config.botKey();
+  return !!this.ferds[botKey];
+};
+
+/**
+ * How do you kill ferd?
+ */
+MegaFerd.prototype.killFerd = function() {
+
+}
 
 module.exports = MegaFerd;
