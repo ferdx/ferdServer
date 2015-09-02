f0d91e6aa115dab821b5472c46d71d96c1bc26e3
David Rosson <david@rosson.com.au>
2015-09-02 12:14:32 -0700

    updated handler method binding

--- api/handlers.js
+++ api/handlers.js
@@ -19,8 +19,9 @@ handlers.pack = function (data) {
 
 handlers.fetch = function (req, res) {
   // interrogate the database and get a promise
+  var handlers = this;
   db.findOne(req.body.username)
-    .then(this.pack.bind(this)); 
+    .then(handlers.pack); 
 };
 
 module.exports = handlers;
